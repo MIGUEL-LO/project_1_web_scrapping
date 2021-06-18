03714970be21148bf01f3175a920e47cfd8dc24e
Miguel Lopez <miguel.birbuet.19@ucl.ac.uk>
2021-06-18 14:21:50 +0100

    making it autmated fight night 1 vs 1

--- UFC_data.py
+++ UFC_data.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import time
 from selenium import webdriver
 import pprint
@@ -7,32 +7,46 @@ driver = webdriver.Chrome(executable_path='C:\\Users\\migue\\Downloads\\chromedr
 URL = "http://www.ufcstats.com/statistics/events/completed"
 driver.get(URL)
 #%%
-# find all fight events in history available.
+# find all fight night events in the first page of history available.
 fight_events_links = driver.find_elements_by_css_selector(".b-statistics__table-content [href]")
-# # each fight night event has a link where multiple fights occured.
-# # Get access to the even containing these fights.
-# # to then access indvidual fights and pull info out
+# each fight night event has a link where multiple fights occured.
+# Get access to the event containing these fights.
+# to then access indvidual fights and pull info out
 fight_event_links_ls = [fight_event_link.get_attribute('href') for fight_event_link in fight_events_links]
+print("-------------")
+fight_event_links_ls = fight_event_links_ls[1:]
+print("fight night events links")
 print(fight_event_links_ls)
 print(len(fight_event_links_ls))
 
 
+# %%
+all_fights_at_an_event_night = []
+for fight_night_event_link in fight_event_links_ls:
+    driver.get(fight_night_event_link)
+    all_fights_at_an_event_night.append(driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr"))
 
-# fighters can draw not always green box
-# fix
-# for i in range(1,2): #len(fight_event_links_ls)):
-    # print("index = ",  i)
-    # print(fight_event_links_ls[i])
-    # print("============")
-driver.get('http://www.ufcstats.com/event-details/0b64d0fed453ef7f')
-#     # driver.get(fight_event_links_ls[i])
-fights_at_an_event = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
-count = 0
-for fight in fights_at_an_event:
-    count += 1
-    print(fight.get_attribute("data-link"))
-    print("count = ", count)
-    print("--------------")
+print('-------')
+print(all_fights_at_an_event_night)
+# fight_1_vs_1_links = []
+
+# for fight_1_vs_1 in all_fights_at_an_event_night:
+#     fight_1_vs_1_links.append(fight_1_vs_1.get_attribute("data-link"))
+
+# print("-------")
+# print("all fights 1 vs 1 links:\n")
+# print(fight_1_vs_1_links)
+# # # fighters can draw not always green box
+# for i in range 
+# driver.get('http://www.ufcstats.com/event-details/0b64d0fed453ef7f')
+# #     # driver.get(fight_event_links_ls[i])
+# fights_at_an_event = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+# count = 0
+# for fight in fights_at_an_event:
+#     count += 1
+#     print(fight.get_attribute("data-link"))
+#     print("count = ", count)
+#     print("--------------")
 # for fight in fights_at_an_event:
 #     print(fight.text)
 #     driver.quit()
@@ -44,3 +58,4 @@ for fight in fights_at_an_event:
 # print(fight_event_links_ls)
 # print(fights_data_loc_link)
 # print(len(fights_data_loc_link))
+# %%
