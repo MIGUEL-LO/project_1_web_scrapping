02f37943377328c94a2ef426a54c39f1850b5a04
Miguel Lopez <miguel.birbuet.19@ucl.ac.uk>
2021-06-18 19:05:01 +0100

    Finds the links to all the fights 1 vs 1, from which needs to extract fight stats

--- UFC_data.py
+++ UFC_data.py
@@ -2,60 +2,64 @@
 import time
 from selenium import webdriver
 import pprint
-#%%
 driver = webdriver.Chrome(executable_path='C:\\Users\\migue\\Downloads\\chromedriver_win32\\chromedriver.exe')
+# URL to all fight events in the past
 URL = "http://www.ufcstats.com/statistics/events/completed"
 driver.get(URL)
-#%%
-# find all fight night events in the first page of history available.
+# Contains the links of fight night events that occur at a given night.
+# So each event/night contains multiple fights
 fight_events_links = driver.find_elements_by_css_selector(".b-statistics__table-content [href]")
-# each fight night event has a link where multiple fights occured.
-# Get access to the event containing these fights.
-# to then access indvidual fights and pull info out
+print(fight_events_links)
+# Get the links to these fight night events containing multiple fights.
 fight_event_links_ls = [fight_event_link.get_attribute('href') for fight_event_link in fight_events_links]
 print("-------------")
+# The first row is for upcoming fights, ignore it.
 fight_event_links_ls = fight_event_links_ls[1:]
 print("fight night events links")
 print(fight_event_links_ls)
 print(len(fight_event_links_ls))
 
-
 # %%
-all_fights_at_an_event_night = []
-for fight_night_event_link in fight_event_links_ls:
-    driver.get(fight_night_event_link)
-    all_fights_at_an_event_night.append(driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr"))
+# contains all links for the fights. the links contain the stats of that fight.
+all_fights_links_1_vs_1 = []
+counts = []
+for fight_night_event_idx in range(0, len(fight_event_links_ls)):
+    count = 0
+    link_fight_night = fight_event_links_ls[fight_night_event_idx]
+    # finding the elements with the class to find the link for particular fight.
+    driver.get(link_fight_night)
+    fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+    for fight_1_vs_1_link in fights_links_in_fight_night:
+        count += 1
+        # contains all fight 1 vs 1 links from which will get stats.
+        all_fights_links_1_vs_1.append(fight_1_vs_1_link.get_attribute("data-link"))
+    counts.append(count)
+# %%
+# if sum of counts which indicate how many links there are belonging to a fight
+# is equal to the amount of links in all_fights_at_an_event_night
+# then we good 
+print("sum_counts = ", sum(counts))
+print(len(all_fights_links_1_vs_1))
 
-print('-------')
-print(all_fights_at_an_event_night)
-# fight_1_vs_1_links = []
 
-# for fight_1_vs_1 in all_fights_at_an_event_night:
-#     fight_1_vs_1_links.append(fight_1_vs_1.get_attribute("data-link"))
 
-# print("-------")
-# print("all fights 1 vs 1 links:\n")
-# print(fight_1_vs_1_links)
-# # # fighters can draw not always green box
-# for i in range 
-# driver.get('http://www.ufcstats.com/event-details/0b64d0fed453ef7f')
-# #     # driver.get(fight_event_links_ls[i])
-# fights_at_an_event = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
-# count = 0
-# for fight in fights_at_an_event:
-#     count += 1
-#     print(fight.get_attribute("data-link"))
-#     print("count = ", count)
-#     print("--------------")
-# for fight in fights_at_an_event:
-#     print(fight.text)
-#     driver.quit()
 
-# fight_1_1 = fights_at_an_event.get_attribute("data-link")
-# fight_event_links_ls = [fight_event_link.get_attribute('data-link') for fight_event_link in fights_at_an_event]
 
-    # fights_data_loc_link = [fight_1_vs_1.get_attribute('href') for fight_1_vs_1 in fights_at_an_event]
-# print(fight_event_links_ls)
-# print(fights_data_loc_link)
-# print(len(fights_data_loc_link))
+
+#######################
+#######################
+
+# for fight_night_event_idx in range(0,2):
+#     count = 0
+#     link_fight_night = fight_event_links_ls[fight_night_event_idx]
+#     # print(link_fight_night)
+#     # print(type(fight_event_links_ls[fight_night_event_idx]))
+#     driver.get(link_fight_night)
+#     fights_in_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+#     for fight in fights_in_night:
+#         # count += 1
+#         all_fights_at_an_event_night.append(fight.get_attribute("data-link"))
+#     # print("count = ", count)
+# print(all_fights_at_an_event_night)
+
 # %%
