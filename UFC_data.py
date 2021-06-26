cdaeef464d18ef3ad92609521e63dee7998e1d0d
Miguel <ucaplop@ucl.ac.uk>
2021-06-27 00:25:46 +0100

    finding names and stats

--- UFC_data.py
+++ UFC_data.py
@@ -1,10 +1,11 @@
 # %%
-import time
+from typing import final
 from selenium import webdriver
-import pprint
+import concurrent.futures
+
 def links_to_fight_night_events(URL="http://www.ufcstats.com/statistics/events/completed"):
 
-    executable_path = 'C:\\Users\\migue\\Downloads\\chromedriver_win32\\chromedriver.exe'
+    executable_path = '/usr/bin/chromedriver'
 
     driver = webdriver.Chrome(executable_path=executable_path)
     # URL to all fight events in the past
@@ -27,51 +28,129 @@ def links_to_fight_night_events(URL="http://www.ufcstats.com/statistics/events/c
 # contains all links for the fights. the links contain the stats of that fight.
 def links_to_fights_stats_1_vs_1(fight_event_links_ls, driver):
 
-    all_fights_links_1_vs_1 = []
-    counts = []
-    for fight_night_event_link in fight_event_links_ls:
-        count = 0
-        # link_fight_night = fight_event_links_ls[fight_night_event_idx]
-        # finding the elements with the class to find the link for particular fight.
-        driver.get(fight_night_event_link)
-        fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
-        for fight_1_vs_1_link in fights_links_in_fight_night:
-            count += 1
-            # contains all fight 1 vs 1 links from which will get stats.
-            all_fights_links_1_vs_1.append(fight_1_vs_1_link.get_attribute("data-link"))
-        counts.append(count)
+    try :
+
+        all_fights_links_1_vs_1 = []
+        counts = []
+        for fight_night_event_link in fight_event_links_ls:
+            count = 0
+            # link_fight_night = fight_event_links_ls[fight_night_event_idx]
+            # finding the elements with the class to find the link for particular fight.
+            driver.get(fight_night_event_link)
+            fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+            for fight_1_vs_1_link in fights_links_in_fight_night:
+                count += 1
+                # contains all fight 1 vs 1 links from which will get stats.
+                all_fights_links_1_vs_1.append(fight_1_vs_1_link.get_attribute("data-link"))
+            counts.append(count)
+        
+        if sum(counts) == len(all_fights_links_1_vs_1):
+            return all_fights_links_1_vs_1
+        else:
+            return 0
+    finally:
+        driver.quit()
+
+
+
+#############################
+#############################
+#############################
+#############################
+
+def fight_link_in_fight_night(fight_event_link, driver):
+
+    try:
+        all_fights_links_1_vs_1 = []
+        counts = []        
+        driver.get(fight_event_link)
+        fight_links_in_fight_night_ = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+
+    finally:
+        driver.quit()
     
-    if sum(counts) == len(all_fights_links_1_vs_1):
-        return all_fights_links_1_vs_1
-    else:
-        return 0
+    return fight_link_in_fight_night_
+
+
+def links_to_fights_stats_1_vs_1(fight_event_links_ls, driver):
+    
+    try :
+
+        all_fights_links_1_vs_1 = []
+        counts = []
+        for fight_night_event_link in fight_event_links_ls:
+            count = 0
+            # link_fight_night = fight_event_links_ls[fight_night_event_idx]
+            # finding the elements with the class to find the link for particular fight.
+            driver.get(fight_night_event_link)
+            fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+            for fight_1_vs_1_link in fights_links_in_fight_night:
+                count += 1
+                # contains all fight 1 vs 1 links from which will get stats.
+                all_fights_links_1_vs_1.append(fight_1_vs_1_link.get_attribute("data-link"))
+            counts.append(count)
+        
+        if sum(counts) == len(all_fights_links_1_vs_1):
+            return all_fights_links_1_vs_1
+        else:
+            return 0
+    finally:
+        driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################
+#############################
+#############################
+#############################
 
 
 def links_to_fights_stats_1_vs_1_test_range(fight_event_links_ls, driver):
+    try:
+        all_fights_links_1_vs_1 = []
+        counts = []
+        fight_event_links_ls = fight_event_links_ls[1:]
+        # for fight_night_event_idx in range(1, len(fight_event_links_ls)):
+        #     count = 0
+        #     link_fight_night = fight_event_links_ls[fight_night_event_idx]
+        for link_fight_night in fight_event_links_ls:
+            count = 0
+            # finding the elements with the class to find the link for particular fight.
+            driver.get(link_fight_night)
+            fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
+            for fight_1_vs_1_link in fights_links_in_fight_night:
+                count += 1
+                # contains all fight 1 vs 1 links from which will get stats.
+                all_fights_links_1_vs_1.append(fight_1_vs_1_link.get_attribute("data-link"))
+            counts.append(count)
 
-    all_fights_links_1_vs_1 = []
-    counts = []
-    for fight_night_event_idx in range(0, 1):
-        count = 0
-        link_fight_night = fight_event_links_ls[fight_night_event_idx]
-        # finding the elements with the class to find the link for particular fight.
-        driver.get(link_fight_night)
-        fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
-        for fight_1_vs_1_link in fights_links_in_fight_night:
-            count += 1
-            # contains all fight 1 vs 1 links from which will get stats.
-            all_fights_links_1_vs_1.append(fight_1_vs_1_link.get_attribute("data-link"))
-        counts.append(count)
-
-    if sum(counts) == len(all_fights_links_1_vs_1):
-        return all_fights_links_1_vs_1
-    else:
-        return 0
+        if sum(counts) == len(all_fights_links_1_vs_1):
+            return all_fights_links_1_vs_1
+        else:
+            return 0
+    finally:
+        driver.quit()
 # %%
 
 
-# links_to_fight_night_events, driver = links_to_fight_night_events()
-# fight_links = links_to_fights_stats_1_vs_1_test_range(links_to_fight_night_events, driver)
+links_to_fight_night_events_, driver = links_to_fight_night_events()
+# print(links_to_fight_night_events_)
+fight_links_stats = links_to_fights_stats_1_vs_1_test_range(links_to_fight_night_events_, driver)
+print(fight_links_stats)
 # print(fight_links)
 # print(len(fight_links))
 
--- finding_fight_stats.py
+++ finding_fight_stats.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-executable_path = 'C:\\Users\\migue\\Downloads\\chromedriver_win32\\chromedriver.exe'
+executable_path = '/usr/bin/chromedriver'
 driver = webdriver.Chrome(executable_path=executable_path)
 URL = 'http://www.ufcstats.com/fight-details/6cd44e1b2d093ea4'
 driver.get(URL)
@@ -18,21 +18,33 @@ significant_strikes_vals = []
 #%%
 try:
     page_container = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "l-page__container")))
+
     fight_details = page_container.find_element_by_xpath("//div[@class='b-fight-details']")
     fight_details_person = fight_details.find_elements_by_class_name("b-fight-details__person")
+
     weight_division = fight_details.find_element_by_class_name("b-fight-details__fight-head")
     weight_division = weight_division.find_element_by_tag_name("i")
-    totals = page_container.find_element_by_xpath("//section[@class='b-fight-details__section js-fight-section']")
-    tots_headers = totals.find_element_by_xpath("//table//thead/tr")
-    striking_columns = tots_headers.find_elements_by_xpath("th")
-    fight_scores = totals.find_element_by_xpath("//table//tbody/tr")
-    fighter_names = fight_scores.find_elements_by_tag_name("a")
-    fight_stats_tots = fight_scores.find_elements_by_xpath("td")
-    significant_strikes_header = page_container.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']")[1]
-    sig_fight_scores = totals.find_element_by_xpath("//table//tbody/tr")
-    sig_fighter_names = sig_fight_scores.find_elements_by_tag_name("a")
-    sig_fight_stats_tots = sig_fight_scores.find_elements_by_xpath("td")
 
+
+    total_strikes_sig_strikes_headers_ele = page_container.find_elements_by_xpath("//thead[@class='b-fight-details__table-head']")
+    total_strikes_headers_ele, sig_strikes_headers_ele = total_strikes_sig_strikes_headers_ele[0]
+     
+    print(total_strikes_sig_strikes_headers_ele, "\n")
+    total_strikes_sig_strikes_stats_ele = page_container.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']")
+    print(total_strikes_sig_strikes_stats_ele, "\n")
+# 
+    # striking_columns = tots_headers.find_elements_by_xpath("th")
+    # fight_scores = totals.find_element_by_xpath("//table//tbody/tr")
+# 
+    # fighter_names = fight_scores.find_elements_by_tag_name("a")
+    # fight_stats_tots = fight_scores.find_elements_by_xpath("td")
+    # 
+    # significant_strikes_header = page_container.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']")[1]
+    # sig_fight_scores = totals.find_element_by_xpath("//table//tbody/tr")
+    # 
+    # sig_fighter_names = sig_fight_scores.find_elements_by_tag_name("a")
+    # sig_fight_stats_tots = sig_fight_scores.find_elements_by_xpath("td")
+# 
 
 
 
@@ -71,33 +83,33 @@ try:
     # for table in significant_strikes_header:
     #     print(table.text)
 ############################
-    for fighter_detail in fight_details_person:
-        # Fight result and corresponding fighter name
-        w_or_l = fighter_detail.find_element_by_tag_name("i").text
-        name = fighter_detail.find_element_by_tag_name("a").text
-        w_or_l_fighter_name.append((w_or_l, name))
+    # for fighter_detail in fight_details_person:
+    #     # Fight result and corresponding fighter name
+    #     w_or_l = fighter_detail.find_element_by_tag_name("i").text
+    #     name = fighter_detail.find_element_by_tag_name("a").text
+    #     w_or_l_fighter_name.append((w_or_l, name))
 
-    for col in striking_columns:
-        total_strike_headers.append(col.text)
+    # for col in striking_columns:
+    #     total_strike_headers.append(col.text)
 
-    for name in fighter_names:
-        fighter_name_total_strike.append(name.text)
+    # for name in fighter_names:
+    #     fighter_name_total_strike.append(name.text)
 
-    for fight_stat in fight_stats_tots:
-        # print(fight_stat)
-        stat = fight_stat.find_elements_by_tag_name("p")
-        for s in stat:
-            # print(s.text)
-            fighter_striking_stats.append(s.text)
+    # for fight_stat in fight_stats_tots:
+    #     # print(fight_stat)
+    #     stat = fight_stat.find_elements_by_tag_name("p")
+    #     for s in stat:
+    #         # print(s.text)
+    #         fighter_striking_stats.append(s.text)
 
-    for col_name in sig_fighter_names:
-        significant_strikes_col_names.append(col_name.text)
+    # for col_name in sig_fighter_names:
+    #     significant_strikes_col_names.append(col_name.text)
 
-    for fight_stat in sig_fight_stats_tots:
-        stat = fight_stat.find_elements_by_tag_name("p")
-        for s in stat:
-            # print(s.text)
-            significant_strikes_vals.append(s.text)
+    # for fight_stat in sig_fight_stats_tots:
+    #     stat = fight_stat.find_elements_by_tag_name("p")
+    #     for s in stat:
+    #         # print(s.text)
+    #         significant_strikes_vals.append(s.text)
 
 
 
@@ -109,9 +121,11 @@ try:
 finally:
     driver.quit()
 
-print(w_or_l_fighter_name)
-print(total_strike_headers)
-print(fighter_name_total_strike)
-print(fighter_striking_stats)
-print(significant_strikes_col_names)
-print(significant_strikes_vals)
+#%%
+# print(w_or_l_fighter_name, "\n")
+# print(total_strike_headers, "\n")
+# print(fighter_name_total_strike, "\n")
+# print(fighter_striking_stats, "\n")
+# print(significant_strikes_col_names, "\n")
+# print(significant_strikes_vals, "\n")
+# # %%
