2714b7a626703dc68c51f0769420d85809c2366c
Miguel <ucaplop@ucl.ac.uk>
2021-06-28 00:39:39 +0100

    adds documentation

--- UFC_data.py
+++ UFC_data.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-from IPython.display import display
 from sqlalchemy import create_engine
 
 
@@ -35,8 +34,7 @@ class UFC:
         fight_event_links_ls = [fight_event_link.get_attribute('href') for fight_event_link in fight_events_links]
         fight_event_links_ls = fight_event_links_ls[1:]
 
-        return fight_event_links_ls, driver 
-
+        return fight_event_links_ls, driver
 
     def links_to_fights_stats_1_vs_1(self, fight_event_links_ls, driver):
         '''
@@ -48,7 +46,6 @@ class UFC:
         counts = []
         for fight_night_event_link in fight_event_links_ls:
             count = 0
-            # link_fight_night = fight_event_links_ls[fight_night_event_idx]
             # finding the elements with the class to find the link for particular fight.
             driver.get(fight_night_event_link)
             fights_links_in_fight_night = driver.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']/tr")
@@ -62,8 +59,7 @@ class UFC:
             return all_fights_links_1_vs_1, driver
         else:
             return 0
-    
-    
+
     def get_fight_stats(self, URL, driver):
         '''
         Scrapes the fighting stats for a given fight.
@@ -71,98 +67,83 @@ class UFC:
         The percentages of landed hits in a given location either head, body or legs.
         Also includes submission stats.
         '''
-        
+
         driver.get(URL)
         w_or_l = []
         fighter_name = []
-    
+
         total_strike_headers = []
         first_fighter_tot_strikes = []
         second_fighter_tot_strikes = []
-    
+
         sig_strike_headers = []
         first_fighter_sig_strikes = []
         second_fighter_sig_strikes = []
-        
+
         landed_by_target_headers = []
         first_fighter_landed_by_target = []
         second_fighter_landed_by_target = []
-        
+
         landed_by_position_headers = []
         first_fighter_landed_by_position = []
         second_fighter_landed_by_position = []
-    
-    
+
         page_container = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "l-page__container")))
-    
+
         fight_details = page_container.find_element_by_xpath("//div[@class='b-fight-details']")
         fight_details_person = fight_details.find_elements_by_class_name("b-fight-details__person")
-    
+
         weight_division = [fight_details.find_element_by_class_name("b-fight-details__fight-title").text]
-    
+
         for fighter_detail in fight_details_person:
-                # Fight result and corresponding fighter name
+            # Fight result and corresponding fighter name
             w_or_l_ = fighter_detail.find_element_by_tag_name("i").text
             name = fighter_detail.find_element_by_tag_name("a").text
-            # w_or_l_fighter_name.append((w_or_l, name))
             w_or_l.append(w_or_l_)
             fighter_name.append(name)
-        first_fighter_w_or_l =  [w_or_l[0]]
+        first_fighter_w_or_l = [w_or_l[0]]
         first_fighter_name = [fighter_name[0]]
-        second_fighter_w_or_l =  [w_or_l[1]]
+        second_fighter_w_or_l = [w_or_l[1]]
         second_fighter_name = [fighter_name[1]]
-        # print(w_or_l_fighter_name, "erm")
-    
-    
-    
+
         # headers for tots and sig strikes
         # stats belonging to headers
         total_strikes_sig_strikes_headers = page_container.find_elements_by_xpath("//thead[@class='b-fight-details__table-head']")    
-        # print(total_strikes_sig_strikes_headers)
         total_strikes_headers_eles = total_strikes_sig_strikes_headers[0]
         total_strikes_headers_eles_indiv = total_strikes_headers_eles.find_elements_by_tag_name("th")
-    # 
+
         for tot_header in total_strikes_headers_eles_indiv:
             total_strike_headers.append(tot_header.text)
-    # 
+
         sig_strikes_headers_eles = total_strikes_sig_strikes_headers[1]
         sig_strikes_headers_eles_indiv = sig_strikes_headers_eles.find_elements_by_tag_name("th")
-    # 
+
         for sig_header in sig_strikes_headers_eles_indiv:
             sig_strike_headers.append(sig_header.text)
-    # 
-    # URL, driver
+
         total_strikes_sig_strikes_stats_ele = page_container.find_elements_by_xpath("//tbody[@class='b-fight-details__table-body']")    
-        total_strikes_stats_ele =  total_strikes_sig_strikes_stats_ele[0]
+        total_strikes_stats_ele = total_strikes_sig_strikes_stats_ele[0]
         total_strikes_stats_ele_vals = total_strikes_stats_ele.find_elements_by_tag_name("p")
-    
-    
+
         for i, stat_val in enumerate(total_strikes_stats_ele_vals):
             if i % 2 == 0:
                 first_fighter_tot_strikes.append(stat_val.text)
             else:
                 second_fighter_tot_strikes.append(stat_val.text)
-    
-        # print(first_fighter_tot_strikes, "\n")
-        # print(second_fighter_tot_strikes, "\n")
-    
-    
+
         sig_strikes_stats_ele = total_strikes_sig_strikes_stats_ele[2]
         sig_strikes_stats_ele_vals = sig_strikes_stats_ele.find_elements_by_tag_name("p")
-    
+
         for i, stat_val in enumerate(sig_strikes_stats_ele_vals):
             if i % 2 == 0:
                 first_fighter_sig_strikes.append(stat_val.text)
             else:
                 second_fighter_sig_strikes.append(stat_val.text)
-        # print(first_fighter_sig_strikes, "\n")
-        # print(second_fighter_sig_strikes, "\n")
-    
+
         strikes_landed_by_target_postion = page_container.find_elements_by_xpath("//div[@class='b-fight-details__charts-row']")
         strikes_landed_by_target = strikes_landed_by_target_postion[:3]    
         strikes_landed_by_position = strikes_landed_by_target_postion[3:]
-    
-    
+
         for target in strikes_landed_by_target:
             strikes_landed_by_target = target.find_elements_by_tag_name("i")
             strikes_landed_by_target_fighter_1 = strikes_landed_by_target[0]
@@ -171,12 +152,7 @@ class UFC:
             landed_by_target_headers.append(strikes_landed_by_target_names_h_b_l.text)
             first_fighter_landed_by_target.append(strikes_landed_by_target_fighter_1.text)
             second_fighter_landed_by_target.append(strikes_landed_by_target_fighter_2.text)
-    
-        # print("targ = ", landed_by_target_headers, "\n")
-        # print(first_fighter_landed_by_target, "\n")
-        # print(second_fighter_landed_by_target, "\n")
-    
-    
+
         for possition in strikes_landed_by_position:
             strikes_landed_by_position = possition.find_elements_by_tag_name("i")
             strikes_landed_by_position_fighter_1 = strikes_landed_by_position[0]
@@ -185,14 +161,7 @@ class UFC:
             landed_by_position_headers.append(strikes_landed_by_position_names_d_c_g.text)
             first_fighter_landed_by_position.append(strikes_landed_by_position_fighter_1.text)
             second_fighter_landed_by_position.append(strikes_landed_by_position_fighter_2.text)
-    
-        # print("targ = ", landed_by_position_headers, "\n")
-        # print(first_fighter_landed_by_position, "\n")
-        # print(second_fighter_landed_by_position, "\n")
-    
-    
-        # driver.quit()
-    
+
         return weight_division, first_fighter_w_or_l, second_fighter_w_or_l, first_fighter_name, second_fighter_name, total_strike_headers, first_fighter_tot_strikes, second_fighter_tot_strikes, sig_strike_headers, first_fighter_sig_strikes, second_fighter_sig_strikes, landed_by_target_headers, first_fighter_landed_by_target, second_fighter_landed_by_target, landed_by_position_headers, first_fighter_landed_by_position, second_fighter_landed_by_position
 
     def creating_UFC_df(self, fight_links_stats, driver):
@@ -214,7 +183,7 @@ class UFC:
                     first_row = [id] + weight_division + first_fighter_w_or_l + first_fighter_name + first_fighter_tot_strikes + first_fighter_sig_strikes + first_fighter_landed_by_target + first_fighter_landed_by_position
                     second_row = [id] + weight_division + second_fighter_w_or_l + second_fighter_name + second_fighter_tot_strikes + second_fighter_sig_strikes + second_fighter_landed_by_target + second_fighter_landed_by_position
 
-                    columns_headers = ['id' ,'Weight devision', 'Result', 'Fighters name'] + total_strike_headers + sig_strike_headers + landed_by_target_headers + landed_by_position_headers
+                    columns_headers = ['id', 'Weight devision', 'Result', 'Fighters name'] + total_strike_headers + sig_strike_headers + landed_by_target_headers + landed_by_position_headers
                     df = pd.DataFrame(columns=columns_headers)
                     df.loc[0] = first_row
                     df.loc[1] = second_row
@@ -229,15 +198,13 @@ class UFC:
 
                     df = df.append(first_series, ignore_index=True)
                     df = df.append(second_series, ignore_index=True)
-                    # display(df)
-                # i += 1
                 id += 1
             except:
-                print("error at id = ", id)
-            # i += 1
+                # print("error at id = ", id)
+                pass
 
         df = df.drop(columns=['FIGHTER'])
-        df.drop(df.filter(regex="Unname"),axis=1, inplace=True)
+        df.drop(df.filter(regex="Unname"), axis=1, inplace=True)
 
         return df
 
@@ -256,15 +223,17 @@ class UFC:
         engine.connect()
         df.to_sql(name="UFC_data_test", con=engine, schema="public", if_exists="replace", index=False)
 
-        return 0 
+        return 0
+
 
 if __name__ == "__main__":
-    data_UFC = UFC()
+    data_UFC = UFC()  # Instantiate object.
+    # Finds links to all fight night events.
     links_to_fight_night_events_, driver = data_UFC.links_to_fight_night_events()
 
-
+    # Finds links to all fights.
     fight_links_stats, driver = data_UFC.links_to_fights_stats_1_vs_1(links_to_fight_night_events_, driver)
-    # print(fight_links_stats, "\n")
+    # Creates a data frame containing scraped data.
     UFC_df = data_UFC.creating_UFC_df(fight_links_stats, driver)
-    # display(UFC_df)
+    # Uploads data frame to an instance of AWS's RDS.
     upload_db_ = data_UFC.upload_db(UFC_df)
